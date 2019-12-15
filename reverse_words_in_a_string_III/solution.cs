d06522395b0b017c4bdf4032c49d655fc56f5c4a
samhuynhle <samhuynhle@gmail.com>
2019-12-15 02:41:47 -0800

    added comments to reverse words in a string III algorithm

--- reverse_words_in_a_string_III/solution.cs
+++ reverse_words_in_a_string_III/solution.cs
@@ -6,11 +6,12 @@ public class Solution {
         var start = 0;
         var end = 0;
         var sb = new StringBuilder(s);
-        for (var i = 0; i < s.Length; i++)
+        for (var i = 0; i < s.Length; i++) // simply going through each character in the string
         {
-            if (i - 1 >= 0 && s[i] != ' ' && s[i - 1] == ' ') // we only move the start if we reach these three conditions
+            // we only move the start if we reach these three conditions: 1) if we are at the first character, 2) we are not on a white space, 3) the previous character is a white space
+            if (i - 1 >= 0 && s[i] != ' ' && s[i - 1] == ' ') 
             {
-                start = i;
+                start = i; // we reposition the start marker if we are at the start of a word, indicated by the three conditionals
             }
             if (s[i] != ' ' && (i == s.Length - 1 || (i + 1 < s.Length && s[i + 1] == ' '))) // if we reach these two conditions (one condition has an or to account for the end of string), we know we are at an end of a word
             {
